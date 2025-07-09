5993710b004a3b3c9b9781b53ae8cea9793e55af
Aryan Rana <aryanrana@Aryans-MacBook-Air.local>
2025-07-09 12:35:44 +0530

    commit 2

--- day1.cpp
+++ day1.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main(){
     cout<<"hello";
+    cout<<"git add ."<<endl;
+    cout<<"git commit -m \"first commit\""<<endl;
+    cout<<"git push origin main"<<endl;
+    cout<<"git pull origin main"<<endl;
+    cout<<"git status"<<endl;
     return 0;
 }
